5bccc722f1c7c17170dfbd72a497b8dce76b1ca7
Shivam Singhal <49789262+omshiv12@users.noreply.github.com>
2022-02-08 12:02:40 +0530

    Create calc.java

---
+++ calc.java
@@ -0,0 +1,17 @@
+import java.util.*
+public class calc{
+  public static void main(String args[]){
+  	int num1;
+  	int num2;
+  	 Scanner input=new Scanner(System.in);
+  	 System.out.println("enter the first number");
+  	 num1=input.nextInt();
+  	 System.out.println("enter the second number");
+  	 num2=input.nextInt();
+  	 
+  	 int a=num1+num2;
+  	 System.out.println("addtion of number is"+ a);
+  	 int b=num1-num2;
+  	 System.out.println("subtraction of numbers is"+ b);
+  }
+}
